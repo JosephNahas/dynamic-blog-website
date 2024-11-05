b7dd19fe7184dabde650c51663b9ebb7b49093b0
Joseph Nahas <joe_nahas@hotmail.com>
2024-11-05 15:35:33 -0500

    Add responsive styling to all pages

--- script.js
+++ script.js
@@ -14,10 +14,12 @@ function PassIDtoStorage(link){
 }
 
 if (localStorage.getItem('blog-objects') === null){
-    let blog1 = new Blog('title1','title1', 'content1', 'https://image.api.playstation.com/vulcan/img/rnd/202010/2614/NVmnBXze9ElHzU6SmykrJLIV.png');
+    let title1 = 'Lorem ipsum dolor sit amet';
+    let content1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor non ipsum a interdum. In cursus consectetur est, nec aliquet diam sodales lobortis. Maecenas non posuere justo, sit amet gravida dolor. Nunc dolor libero, blandit vitae purus eu, dictum euismod lectus. Nulla fermentum ligula vel metus dignissim consequat. Suspendisse eget mauris id justo porttitor egestas ac ac leo. Suspendisse potenti. In et elit in diam tristique rutrum. Praesent viverra mi velit, id aliquet nunc tincidunt vitae. Quisque at imperdiet erat. Suspendisse malesuada tortor eu massa gravida, sit amet scelerisque neque posuere. Vestibulum at velit orci. Suspendisse convallis blandit tellus cursus mollis.'
+    let blog1 = new Blog(title1,title1, content1, 'https://image.api.playstation.com/vulcan/img/rnd/202010/2614/NVmnBXze9ElHzU6SmykrJLIV.png');
     blogObjects.push(blog1);
     let entry = document.createElement('li');
-    entry.setAttribute('id', 'title1');
+    entry.setAttribute('id', title1);
     blogList.appendChild(entry);
     let a1 = document.createElement('a');
     let linkText = document.createTextNode(blog1.title);
@@ -26,10 +28,12 @@ if (localStorage.getItem('blog-objects') === null){
     a1.href = 'post.html';
     entry.appendChild(a1);
     PassIDtoStorage(a1);
-    let blog2 = new Blog('title2', 'title2', 'content2', 'https://sm.ign.com/t/ign_nordic/review/b/black-myth/black-myth-wukong-review_xcgh.1200.jpg');
+    let title2 = 'Phasellus magna eros, interdum non.';
+    let content2 = 'Nunc sed vulputate arcu. Duis in fermentum ex, at aliquam ante. Pellentesque vel bibendum velit. Aenean vestibulum condimentum faucibus. Aliquam et est sed elit tristique condimentum nec vel nisi. Vivamus iaculis odio sed nisl accumsan iaculis. Donec sit amet libero malesuada, consequat lectus ac, cursus leo. Praesent nulla nisi, vulputate at tincidunt suscipit, ornare eget ligula. Vestibulum blandit volutpat massa quis tempus.';
+    let blog2 = new Blog(title2, title2, content2, 'https://sm.ign.com/t/ign_nordic/review/b/black-myth/black-myth-wukong-review_xcgh.1200.jpg');
     blogObjects.push(blog2);
     let entry2 = document.createElement('li');
-    entry2.setAttribute('id', 'title2');
+    entry2.setAttribute('id', title2);
     blogList.appendChild(entry2);
     let a2 = document.createElement('a');
     let linkText2 = document.createTextNode(blog2.title);
@@ -64,5 +68,12 @@ clearbtn.addEventListener("click", function(event){
     event.preventDefault();
     localStorage.clear();
     window.location.href = "index.html";
+});
+
+let postbtn = document.getElementById('post-btn');
+
+postbtn.addEventListener("click", function(event){
+    event.preventDefault();
+    window.location.href = "new-post.html";
 });
 
